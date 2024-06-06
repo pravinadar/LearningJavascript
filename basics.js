76ab979453ac89a52a1d7b820d209ca88ba8e1da
pravinadar <pnad764@gmail.com>
2024-06-06 17:36:55 +0530

    6/6/24

--- basics.js
+++ basics.js
@@ -27,3 +27,22 @@ let state;
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+
+
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);

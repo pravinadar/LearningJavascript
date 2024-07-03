5685fec566910c9d922f0b83b362666336c97204
pravinadar <pnad764@gmail.com>
2024-07-04 00:32:18 +0530

    4/7/2024

--- 2_Arrays.js
+++ 2_Arrays.js
@@ -66,9 +66,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("pravin"))
+console.log(Array.from("pravin"))
+console.log(Array.from({name: "pravin"})) // interesting
 
 let score1 = 100
 let score2 = 200

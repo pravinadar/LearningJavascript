0d9d00e6b56644df6bbf60e6d6d17bb16c7f432b
pravinadar <pnad764@gmail.com>
2024-07-04 19:42:13 +0530

    4/7/2024

---
+++ 3_Objects.js
@@ -0,0 +1,74 @@
+//Object Literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Pravin",
+    "full name": "Pravin N", 
+    [mySym]: "mykey1",   //[] is used so thst the global mySym is accessed
+    age: 18,
+    location: "Jaipur",
+    email: "Pravin@email.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "myemail@one.com"
+// Object.freeze(JsUser)
+JsUser.email = "myemail@two.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
+
+
+
+// Symbol: Symbol is a built-in object whose constructor returns a unique symbol.
+
+// Symbols are often used as unique property keys in objects.
+
+// mySym is a symbol with the description "key1".
+
+// Object Literal: JsUser is defined using an object literal.
+
+// name: a regular string property.
+
+// "full name": a string property with a space in the key, demonstrating that keys can be more than one word if enclosed in quotes.
+
+// [mySym]: a computed property name using the mySym symbol. 
+
+// This ensures the property is unique and can't be accessed by normal string keys.
+
+// Other properties like age, location, email, isLoggedIn, and lastLoginDays are straightforward key-value pairs.
+
+// Dot Notation: JsUser.email accesses the email property directly.
+
+// Bracket Notation: JsUser["email"] accesses the email property using a string key. 
+
+// This is useful when the key is dynamic or contains special characters.
+
+// Accessing "full name" must be done using bracket notation due to the space in the key.
+
+// Accessing [mySym] is done using bracket notation with the symbol, which ensures the correct unique property is accessed.
+
+// You can modify properties of an object directly unless the object is frozen.
+
+// The square brackets ([]) in object literals allow for dynamic property keys.
+
+// Using [mySym] as a property key accesses the global mySym symbol.
+
+// Symbols ensure that the property key is unique and avoids name collisions.
---
+++ 4_Objects2.js
@@ -0,0 +1,92 @@
+// Another two methods to make objects
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {         // object inside a object inside another object
+            firstname: "nivarp",
+            lastname: "choudhary"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname);  // to access object inside a object inside another object
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// The syntax const obj3 = { obj1, obj2 }; would create 
+// a nested structure where obj3 contains obj1 and obj2 as properties, rather than merging their contents.
+
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj3 = {...obj1, ...obj2}   // spread operation like did for arrays
+// console.log(obj3);
+// Output:
+// {
+//    1: "a",
+//    2: "b",
+//    3: "a",
+//    4: "b"
+// }
+
+
+const users = [
+    {
+        id: 1,
+        email: "myemail@mail.com"
+    },
+    {
+        id: 2,
+        email: "yemail@mail.com"
+    },
+    {
+        id: 3,
+        email: "mail@mail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js",
+    price: "999",
+    courseInstructor: "nivarp"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "nivarp",
+//     "coursename": "js",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
